abe9045c0491be341e0a154b8de7c26585842f6a
JuicyGrape Apps Team <65766625+JuicyGrapeApps@users.noreply.github.com>
2024-03-23 01:19:40 +0000

    Commit removal of unnecessary libraries

--- FamilyFueds/ApplicationControl.cs
+++ FamilyFueds/ApplicationControl.cs
@@ -13,10 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 using Microsoft.Win32;
 using System.Diagnostics;
-using System.Drawing;
-using System.Runtime.Intrinsics.X86;
 
 namespace JuicyGrapeApps.FamilyFueds
 {
--- FamilyFueds/Person.cs
+++ FamilyFueds/Person.cs
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 using JuicyGrapeApps.Core;
 using JuicyGrapeApps.FamilyFueds;
 using System.Diagnostics;
